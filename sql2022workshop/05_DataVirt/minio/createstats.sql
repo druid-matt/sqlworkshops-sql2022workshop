50b3b27a22eddc728fc25b7043fda254f29491e8
Matt Sarrel <59710709+druid-matt@users.noreply.github.com>
2023-12-05 09:36:51 -0800

    Update createstats.sql
    I needed a full db reference to run this.

--- sql2022workshop/05_DataVirt/minio/createstats.sql
+++ sql2022workshop/05_DataVirt/minio/createstats.sql
@@ -1,4 +1,4 @@
 USE [WideWorldImporters];
 GO
-CREATE STATISTICS wwi_ctb_stats ON wwi_customer_transactions_base (CustomerID) WITH FULLSCAN;
+CREATE STATISTICS wwi_ctb_stats ON dbo.wwi_customer_transactions_base (CustomerID) WITH FULLSCAN;
 GO
